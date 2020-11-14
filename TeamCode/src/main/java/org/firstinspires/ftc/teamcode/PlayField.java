c58b64a6bc5f200d453fcb6ae6eb3c908dea05e6
bignaczak <bignaczak@gmail.com>
2020-11-14 07:47:05 -0500

    Added Playfield and verified that merged branched compile. Still need to merge Blinkin and ColorSensor feature branches.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlayField.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PlayField {
+    private final double fieldWidth = 144.0;
+    private final double fieldHeight= 144.0;
+
+    public PlayField(){}
+
+    public double getFieldWidth() {
+        return fieldWidth;
+    }
+
+    public double getFieldHeight() {
+        return fieldHeight;
+    }
+}
