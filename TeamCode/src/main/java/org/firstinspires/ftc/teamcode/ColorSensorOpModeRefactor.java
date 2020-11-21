548cfdfd6d67fa713bdacf534e279cf8dd1ea2f2
bignaczak <bignaczak@gmail.com>
2020-11-21 08:25:36 -0500

    Rebased Feature_AutonCalibration to master after incorporating Ebots_ColorSensor class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorOpModeRefactor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorOpModeRefactor.java
@@ -36,7 +36,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.SensorREVColorDistance;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.ArrayList;
